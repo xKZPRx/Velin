5c656c5ef457736f6ebdf91e95b59faf997217d6
xKZPRx <kacper.wawrz09@gmail.com>
2026-08-23 12:33:15 +0200

    chore: add TypeScript and update .gitignore

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,9 @@
 import { app, BrowserWindow } from "electron";
 import path from 'path';
 
-app.on("ready", ()=> {
+type test = string;
+
+app.on('ready', ()=> {
     const mainWindow = new BrowserWindow({});
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
 })
