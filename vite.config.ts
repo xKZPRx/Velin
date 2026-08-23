11f0ef6f4a8010ee6290d041bc9431255dc6b85f
xKZPRx <kacper.wawrz09@gmail.com>
2026-08-23 12:21:52 +0200

    chore: add Electron main process and update package configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    outDir: "dist-react",
+    outDir: 'dist-react',
   },
 })
